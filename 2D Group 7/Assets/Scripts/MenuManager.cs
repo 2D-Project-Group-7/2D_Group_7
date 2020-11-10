563bd8818ed6ff748d46275ab3635c93c3077d56
JoshuaCCrass <JCCrass3139@gmail.com>
2020-11-10 17:16:15 -0600

    Demo 3

---
+++ 2D Group 7/Assets/Scripts/MenuManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuManager : MonoBehaviour
+{
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene("Level 1");
+
+    }
+
+   // public void ControlsMenu()
+   // {
+        //SceneManager.LoadScene(2);
+   // }
+
+    public void Quit()
+    {
+#if UNITY_STANDALONE
+        Application.Quit();
+#endif
+
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#endif
+    }
+}
